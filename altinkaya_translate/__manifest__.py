333befa76d25030fa60583fe3bcb6a41ec743bf0
Yusuf <yoyosheta@gmail.com>
2024-04-17 14:10:46 +0300

    [MIG] altinkaya_translate

---
+++ altinkaya_translate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Altinkaya Tranlate',
+    'version': '16.0.1.0.0',
+    'summary': "Change Translates",
+    'description': """Change Translates""",
+    'category': 'Partner',
+    'website':'http://www.codequarters.com',
+    'author': 'MehmetAkifOzdemir,Codequarters',
+    'depends': ['sale','stock'],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False
+}
