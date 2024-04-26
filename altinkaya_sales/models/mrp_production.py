39f7ecb742ab52cc14e9ddbe5fa0e6c8394908f1
Yusuf <yoyosheta@gmail.com>
2024-04-26 17:35:49 +0300

    [MIG] altinkaya_sales and dependencies

---
+++ altinkaya_sales/models/mrp_production.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Yousef Sheta (https://github.com/TrueYouface)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+
+
+
+from odoo import models, fields, api
+
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
