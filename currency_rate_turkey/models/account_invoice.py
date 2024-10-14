534fbf5419b43045c6355f501300b407892df93b
yibudak <yibudak@gmail.com>
2024-10-14 11:26:49 +0000

    [FIX] Use proper currency rate on invoice creation

--- currency_rate_turkey/models/account_invoice.py
+++ currency_rate_turkey/models/account_invoice.py
@@ -8,6 +8,9 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def action_move_create(self):
+        """
+        This is necessary to use proper currency rate on move creation
+        """
         new_context = self._context.copy()
         if self.partner_id.property_rate_field != "rate" and not self.use_custom_rate:
             new_context.update(
@@ -18,3 +21,14 @@ class AccountInvoice(models.Model):
         return super(
             AccountInvoice, self.with_context(new_context)
         ).action_move_create()
+
+    @api.model
+    def create(self, vals):
+        """
+        Use proper currency rate on invoice creation
+        """
+        if vals.get("partner_id"):
+            partner = self.env["res.partner"].browse(vals["partner_id"])
+            if partner and partner.property_rate_field != "rate":
+                self = self.with_context(rate_type=partner.property_rate_field)
+        return super(AccountInvoice, self).create(vals)
